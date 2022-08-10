be420e8e9e8ca28bfaacc1f94ea3beb86a3cb690
myatmink <myatminkhant725@gmail.com>
2022-08-10 07:32:15 +0630

    10 august 2022

---
+++ js/app.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+
+	// Start Back to Top
+
+	$('.btn-backtotops').hide();
+	$(window).scroll(function(){
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 370){
+			$('.btn-backtotops').fadeIn(1000);
+		}else{
+			$('.btn-backtotops').fadeOut(1000)
+		};
+	});
+
+	// End Back to Top
+
+	// Start Header
+
+	// Start nav
+
+	$('.navbuttons').click(function(){
+		$('.navbuttons').toggleClass('crossxs');
+	});
+
+	// for nav scroll
+	$(window).scroll(function(){
+
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 200){
+			$('.navbar').addClass('navmenus');
+		}else{
+			$('.navbar').removeClass('navmenus');
+		};
+
+	});
+
+	// End nav
+
+	// End Header
+
+
+	// Start Properties
+
+	$('.propertylists').click(function(){
+
+		// for active item
+
+		// $(this).addClass('activeitems');
+		// $(this).siblings().removeClass('activeitems');
+		$(this).addClass('activeitems').siblings().removeClass('activeitems');
+
+		// for filter
+
+		let getattvalue = $(this).attr('data-filter');
+
+		if(getattvalue === 'all'){
+			$('.filters').show('slide',500);
+		}else{
+
+			$('.filters').hide();
+
+			$('.filters').not('.'+getattvalue).hide('slide',500);
+
+			$('.filters').filter('.'+getattvalue).show('slide',500);
+		}
+
+	});
+
+	// for image overlay (or) lightbox2
+	lightbox.option({
+		showImageNumberLabel:false
+	});
+
+	// End Properties
+
+
+	// Start Adv
+
+	$(window).scroll(function(){
+
+		let getscrolltop = $(this).scrollTop();
+
+		// console.log(getscrolltop);
+
+		if(getscrolltop >= 1100){
+			$('.advimages').addClass('fromlefts');
+			$('.advtexts').addClass('fromrights');
+		}else{
+			$('.advimages').removeClass('fromlefts');
+			$('.advtexts').removeClass('fromrights');
+		};
+
+	});
+
+	// End Adv
+
+	// Start Footer
+
+	const getyear = $('#getyear');
+	const getfullyear = new Date().getUTCFullYear();
+
+	getyear.text(getfullyear);
+
+	// End Footer
+
+});
